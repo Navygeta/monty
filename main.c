62d7ea20f28eb2282e3b61b2db08e90ce23b79e9
RueSibiya <rksibiya1@gmail.com>
2023-10-19 18:43:06 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+* main - Monty language code interpreter
+* @argc: Number of command line arguments
+* @argv: Array of command line args strings
+* Return: Interpret Monty function
+*/
+
+int main(int argc, char *argv[])
+{
+	return interpret_monty(argc, argv);
+}
+
