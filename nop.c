5208e27636c075f755926994d55077dc72336c73
RueSibiya <rksibiya1@gmail.com>
2023-10-19 18:46:05 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  *no_operation- No-operation function for Monty "nop" opcode
+  *@top: Pointer to the top of the stack
+  *@line_number: Unsigned int representing line_number
+  *Return: Void
+ */
+void no_operation(stack_t **top, unsigned int line_number)
+{
+	(void) top; /*Parameter not used*/
+	(void) line_number; /*Parameter not used*/
+}
+
